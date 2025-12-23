582fbc4efe6b026426e1f1d15de1b97100dd9ff4
lzzrhx <casper.borretzen@tuta.com>
2025-12-23 13:22:58 +0100

    Completed section 12: Memory Alignment

--- src/hellogpu.s
+++ src/hellogpu.s
@@ -146,38 +146,6 @@ DrawFlatTriangle:
     jr $ra
     nop
 
-; -----------------------------------------------------------------------------
-; Subroutine to draw a flat-shaded triangle
-; Args:
-; $a0 = IO_BASE_ADDR (IO ports at 0x1F90****)
-; $s0 = Color (for example: 0xBBGGRR)
-; $s1 = x1
-; $s2 = y1
-; $s3 = x2
-; $s4 = y2
-; $s5 = x4
-; $s6 = y3
-; -----------------------------------------------------------------------------
-DrawFlatTriangleOld:
-    ;ori  $t1, $s0, 0x2000  ; Command + Color
-    lui  $t0, 0x2000
-    or   $t1, $t0, $s0
-    sw   $t1, GP0($a0)
-    sll  $s2, $s2, 16      ; Vertex 1
-    andi $s1, $s1, 0xFFFF
-    or   $t1, $s1, $s2
-    sw   $t1, GP0($a0)
-    sll  $s4, $s4, 16      ; Vertex 2
-    andi $s3, $s3, 0xFFFF
-    or   $t1, $s3, $s4
-    sw   $t1, GP0($a0)
-    sll  $s6, $s6, 16      ; Vertex 3
-    andi $s5, $s5, 0xFFFF
-    or   $t1, $s5, $s6
-    sw   $t1, GP0($a0)
-    jr $ra
-    nop
-
 End:
 
 .close
